aa0753a48ac1a2f81db5d2ab3cce612cf96575e2
Leonardo Uchôa Pedreira <leonardo.pedreira.github@samplemed.com.br>
2022-06-06 16:34:11 -0300

    improve func

--- src/custom_transformers/transformers.py
+++ src/custom_transformers/transformers.py
@@ -1,2 +1,29 @@
-def add_one(number):
-    return number + 1
+"""
+Custom scikit-learn transformers.
+"""
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# alternative:
+# https://scikit-learn.org/stable/modules/generated/
+# sklearn.compose.make_column_selector.html
+class FeatureSelector(BaseEstimator, TransformerMixin):
+    """
+    Custom scikit-learn transformer for selecting columns in specified order
+    """
+
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        """
+        fit
+        """
+        return self
+
+    def transform(self, X, y=None):
+        """
+        transform
+        """
+        return X[self.columns]
